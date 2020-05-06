d8fff15157db4f37aaf444359095f559e87165ba
Gabriel Ramos de Sousa <gabrielramos.email@gmail.com>
2020-05-06 15:00:52 -0300

    Web introduction

--- SimpleBank/bank.php
+++ SimpleBank/bank.php
@@ -21,4 +21,25 @@ showMsg("Deposit negative number");
 $checkingAccounts['526-02-9692'] = deposit($checkingAccounts['526-02-9692'], -1000);
 showMsg("------------Deleting a account---------------");
 showAllAccounts();
+?>
 
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Accounts on Simple Bank</h1>
+    <ul>
+        <?php $checkingAccounts = allAccounts();
+        foreach ($checkingAccounts as $account){ ?>
+            <li><?= $account['holder']?> with $<?= $account['amount'] ?> </li>
+        <?php }?>
+    </ul>
+
+</body>
+</html>
